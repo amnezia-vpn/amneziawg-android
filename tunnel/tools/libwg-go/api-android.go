d52f8c47837a9b6866464389a4524eb617dae044
albexk <albexk@proton.me>
2024-02-11 21:53:32 +0300

    Fix go modules naming

--- tunnel/tools/libwg-go/api-android.go
+++ tunnel/tools/libwg-go/api-android.go
@@ -20,11 +20,11 @@ import (
 	"strings"
 	"unsafe"
 
+	"github.com/amnezia-vpn/amneziawg-go/conn"
+	"github.com/amnezia-vpn/amneziawg-go/device"
+	"github.com/amnezia-vpn/amneziawg-go/ipc"
+	"github.com/amnezia-vpn/amneziawg-go/tun"
 	"golang.org/x/sys/unix"
-	"github.com/amnezia-vpn/amnezia-wg/conn"
-	"github.com/amnezia-vpn/amnezia-wg/device"
-	"github.com/amnezia-vpn/amnezia-wg/ipc"
-	"github.com/amnezia-vpn/amnezia-wg/tun"
 )
 
 type AndroidLogger struct {
@@ -213,7 +213,7 @@ func wgVersion() *C.char {
 		return C.CString("unknown")
 	}
 	for _, dep := range info.Deps {
-		if dep.Path == "github.com/amnezia-vpn/amnezia-wg" {
+		if dep.Path == "github.com/amnezia-vpn/amneziawg-go" {
 			parts := strings.Split(dep.Version, "-")
 			if len(parts) == 3 && len(parts[2]) == 12 {
 				return C.CString(parts[2][:7])
--- tunnel/tools/libwg-go/go.mod
+++ tunnel/tools/libwg-go/go.mod
@@ -1,15 +1,15 @@
-module golang.zx2c4.com/wireguard/android
+module github.com/amnezia-vpn/amneziawg-android
 
 go 1.20
 
 require (
-	github.com/amnezia-vpn/amnezia-wg v0.1.8
-	golang.org/x/sys v0.16.0
+	github.com/amnezia-vpn/amneziawg-go v0.2.1
+	golang.org/x/sys v0.17.0
 )
 
 require (
 	github.com/tevino/abool/v2 v2.1.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
 )
