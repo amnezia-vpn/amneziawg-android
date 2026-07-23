4258c90b2184429d688c76f6737fc178ff4f414b
NickVs2015 <nv@amnezia.org>
2026-07-23 09:41:49 +0300

    chore: bump target/compile API level to 36 for Google Play compliance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.android.settings") version "8.3.0"
+    id("com.android.settings") version "8.13.0"
 }
 
 rootProject.name = "amneziawg-android"
@@ -27,8 +27,8 @@ include(":tunnel")
 include(":ui")
 
 configure<SettingsExtension> {
-    buildToolsVersion = "35.0.0"
-    compileSdk = 35
+    buildToolsVersion = "36.0.0"
+    compileSdk = 36
     minSdk = 24
     ndkVersion = "26.1.10909125"
 }
